c59494ec8147a3b5ba58bc3c46b8e158192681d8
Anderley Viana <anderley.viana@gmail.com>
2025-02-05 05:54:15 -0300

    chore: ajuste no admin e botão de planos

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
+from import_export.admin import ImportExportActionModelAdmin
+
 from .models import UserProfile, UsuarioEnvioFormulario
+from pagamentos.models import Pagamentos
 
 
 class UsuarioEnvioFormularioAdmin(admin.ModelAdmin):
@@ -15,8 +17,20 @@ class UserProfileInline(admin.StackedInline):
     verbose_name_plural = 'Dados Complementares'
 
 
-class UserAdmin(BaseUserAdmin):
+class UserAdmin(ImportExportActionModelAdmin):
     inlines = [UserProfileInline]
+    list_display = ['email', 'first_name', 'last_name', 'whatsapp', 'comprou_algum_plano', 'is_staff']
+
+    def comprou_algum_plano(self, obj):
+        if Pagamentos.objects.filter(user=obj, status='pago'):
+            return 'Sim'
+        else:
+            return 'Não'
+
+    def whatsapp(self, obj):
+        user = UserProfile.objects.get(user=obj)
+        if user:
+            return user.whatsapp
 
 
 admin.site.register(UsuarioEnvioFormulario, UsuarioEnvioFormularioAdmin)
